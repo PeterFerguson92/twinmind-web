a07e9b25427ae37410f9352aeef2f8f6cd9d86f3
jrferguson <1q2w3e4r5t6y>
2025-07-26 17:13:32 +0100

    fixed closing item in header

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -2,33 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
+declare var bootstrap: any;
+
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
-    providers: [
-        Location, {
-            provide: LocationStrategy,
-            useClass: PathLocationStrategy
-        }
-    ]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  providers: [
+    Location,
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    }
+  ]
 })
 export class HeaderComponent implements OnInit {
 
-    location: any;
+  currentPath: string = '';
 
-    constructor(
-        public router: Router,
-        location: Location
-    ) { }
+  constructor(
+    public router: Router,
+    private locationService: Location
+  ) {}
 
-    ngOnInit() {
-        this.router.events
-        .subscribe((event) => {
-            if ( event instanceof NavigationEnd ) {
-                this.location = this.router.url;
-            }
-        });
+  ngOnInit(): void {
+    this.currentPath = this.router.url;
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentPath = event.urlAfterRedirects;
+      }
+    });
+  }
+
+  /**
+   * Collapse the navbar on link click (mobile only)
+   */
+  closeNavbar(navbar: HTMLElement): void {
+    if (window.innerWidth < 768 && navbar?.classList.contains('show')) {
+      new bootstrap.Collapse(navbar).hide();
     }
-
+  }
 }
