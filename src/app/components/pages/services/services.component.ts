6f81487f50503dbdb8c44da623d22eae36aefa2e
jrferguson <1q2w3e4r5t6y>
2025-07-26 22:11:33 +0100

    added services

--- src/app/components/pages/services/services.component.ts
+++ src/app/components/pages/services/services.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CommonService } from "src/app/shared/common.service";
 
 @Component({
-  selector: 'app-services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+    selector: "app-services",
+    templateUrl: "./services.component.html",
+    styleUrls: ["./services.component.scss"],
 })
 export class ServicesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor(private commonService: CommonService) {}
+    services = [];
+    
+    ngOnInit() {
+        this.services = this.commonService.getAllServices();
+    }
 }
