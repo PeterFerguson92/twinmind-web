1fbfb2bca547757a12de7186a780777d0a72d8c0
jrferguson <1q2w3e4r5t6y>
2025-07-29 23:19:02 +0100

    added new projects

--- constant.ts
+++ constant.ts
@@ -292,6 +292,40 @@ export const RECENT_PROJECTS = [
     },
     {
         id: 4,
+        name: "MTAG - Website",
+        businessType: "Religion & Spirituality",
+        screenshots: [
+            "assets/img/project-img/mtag-web/mtag-web1.png",
+            "assets/img/project-img/mtag-web/mtag-web2.png",
+            "assets/img/project-img/mtag-web/mtag-web3.png",
+        ],
+        link: null,
+        client: "Miracle Temple (MTAG)",
+        featured: false,
+        shortDescription:
+            "The official website of Miracle Temple Assemblies of God Church — a place to discover our mission, connect with ministries, watch sermons, and grow in faith together.",
+        description:
+            "serves as the digital front door to our church community — welcoming members, visitors, and seekers from around the world. Designed with clarity, warmth, and accessibility in mind, the site offers a central hub for spiritual connection and information. Visitors can explore our beliefs, learn about upcoming events, get involved in ministries, submit prayer requests, and access livestreams or past sermons. Whether you're new to faith or looking for a church home, our website is a gateway to everything happening at Miracle Temple — a church rooted in biblical truth, Spirit-filled worship, and community outreach.",
+    },
+    {
+        id: 5,
+        name: "MTAG - Admin",
+        businessType: "Religion & Spirituality",
+        screenshots: [
+            "assets/img/project-img/mtag-admin/mtag-admin2.png",
+            "assets/img/project-img/mtag-admin/mtag-admin1.png",
+            "assets/img/project-img/mtag-admin/mtag-admin3.png",
+        ],
+        link: null,
+        client: "Miracle Temple (MTAG)",
+        featured: false,
+        shortDescription:
+            "The Miracle Temple Admin Portal is a secure, web-based admin application designed to empower church leadership and staff to easily manage the public-facing website and streamline internal church operations.",
+        description:
+            "is a comprehensive web-based system built to support the digital needs of Miracle Temple Assemblies of God Church. Designed for ease of use, the platform allows authorized church leaders and staff to manage all aspects of the public website — including events, sermons, announcements, and media — as well as handle church member administration with confidence and care. With features like role-based user access, member profiles, attendance tracking, internal communication tools, and real-time content publishing, the admin portal centralizes operations and simplifies day-to-day church management. Whether updating homepage banners or following up on prayer requests, this all-in-one application ensures the church stays organized, connected, and focused on ministr",
+    },
+    {
+        id: 6,
         name: "Seko City Logistics",
         businessType: "Logistics & Shipping",
         screenshots: [
@@ -308,7 +342,7 @@ export const RECENT_PROJECTS = [
             "is a trusted logistics company specializing in door-to-door delivery services from the UK to Ghana. Whether you're sending personal packages, household items, or commercial freight, Seko City ensures your goods are collected, shipped, cleared through customs, and delivered directly to your recipient’s doorstep in Ghana. With transparent pricing, professional customer service, and a focus on reliability, they simplify cross-border shipping for individuals, families, and businesses. Seko City’s mission is to make international delivery feel as easy as local transport — secure, efficient, and stress-free",
     },
     {
-        id: 5,
+        id: 7,
         name: "Seko Admin Application",
         businessType: "Logistics & Shipping",
         screenshots: [
@@ -325,7 +359,7 @@ export const RECENT_PROJECTS = [
             "The Admin Application is a centralized logistics management system purpose-built for Seko City's operations team. It enables staff to efficiently handle customer bookings, track active and past orders, and manage user profiles within the delivery pipeline. With intuitive dashboards and role-based access, the platform ensures secure, real-time visibility across shipping statuses, payment confirmations, and dispatch logs. Designed to support the logistical demands of international shipping—particularly between the UK and Ghana—the admin panel plays a critical role in enhancing operational accuracy, customer service, and business scalability.",
     },
     {
-        id: 6,
+        id: 8,
         name: "Seko Driver Mobile App",
         businessType: "Logistics & Shipping",
         screenshots: [
@@ -342,24 +376,7 @@ export const RECENT_PROJECTS = [
             "The Seko Driver Mobile Application is designed specifically for drivers working with Seko City's logistics network. It streamlines the item pickup process by providing drivers with live access to their assigned tasks, customer contact details, pickup addresses, and delivery routes. The app includes features like live status updates, photo capture for proof of collection, digital signatures, and navigation integration. By connecting seamlessly with the Seko Admin system, the driver app ensures transparency, efficiency, and accountability at every stage of the delivery lifecycle—from the customer's door in the UK to final processing for shipment to Ghana.",
     },
     {
-        id: 7,
-        name: "Jacob weds Vivian",
-        businessType: "Wedding & Event Planning",
-        screenshots: [
-            "assets/img/project-img/jacvivian/jacviv1.png",
-            "assets/img/project-img/jacvivian/jacviv2.png",
-            "assets/img/project-img/jacvivian/jacviv3.png",
-        ],
-        link: "https://jacobwedsvivian.com/",
-        client: "Jacob & Vivian",
-        featured: false,
-        shortDescription:
-            "A beautifully designed wedding website for Jacob and Vivian, providing guests with event details, RSVP functionality, and a glimpse into their love story.",
-        description:
-            "This project is a custom wedding website built for Jacob and Vivian to celebrate and share their special day with loved ones. The site includes a romantic and elegant design, featuring the couple’s story, event schedule, location details, RSVP form, gallery, and guest information. Mobile-responsive and user-friendly, the site ensures guests have everything they need at their fingertips. It also includes subtle animations and soft colors to match the wedding theme, making it a memorable digital companion to their real-life celebration. Built with modern front-end technologies, this project highlights design sensitivity and attention to detail for personal milestone events.",
-    },
-    {
-        id: 8,
+        id: 9,
         name: "Potter's community Network",
         businessType: "Religion & Spirituality",
         screenshots: [
@@ -374,6 +391,23 @@ export const RECENT_PROJECTS = [
             "A modern, responsive website built for PCN, a growing Christian ministry with a digital-first approach and physical presence in Bradford and Milton Keynes.",
         description:
             "The site was crafted with user engagement in mind, highlighting upcoming events, media content, and ministry resources. It also reflects the organization's dual nature: a strong online presence complemented by a local branch in Bradford and a prayer club in Milton Keynes.",
+    },
+    {
+        id: 10,
+        name: "Jacob weds Vivian",
+        businessType: "Wedding & Event Planning",
+        screenshots: [
+            "assets/img/project-img/jacvivian/jacviv1.png",
+            "assets/img/project-img/jacvivian/jacviv2.png",
+            "assets/img/project-img/jacvivian/jacviv3.png",
+        ],
+        link: "https://jacobwedsvivian.com/",
+        client: "Jacob & Vivian",
+        featured: false,
+        shortDescription:
+            "A beautifully designed wedding website for Jacob and Vivian, providing guests with event details, RSVP functionality, and a glimpse into their love story.",
+        description:
+            "This project is a custom wedding website built for Jacob and Vivian to celebrate and share their special day with loved ones. The site includes a romantic and elegant design, featuring the couple’s story, event schedule, location details, RSVP form, gallery, and guest information. Mobile-responsive and user-friendly, the site ensures guests have everything they need at their fingertips. It also includes subtle animations and soft colors to match the wedding theme, making it a memorable digital companion to their real-life celebration. Built with modern front-end technologies, this project highlights design sensitivity and attention to detail for personal milestone events.",
     },
     // {
     //     id: 9,
